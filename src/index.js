8373ea34eee3cb7f4fb39f1dd4169e60b3a80bf9
Luna <lunaypark@gmail.com>
2022-03-26 17:16:17 -0400

    Updated README

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-// https://javascript.plainenglish.io/create-a-react-app-from-scratch-in-2021-8e9948602e9c
 const App = () => {
     return <h1>Initial Test</h1>
 }
