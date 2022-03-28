3b2995cbbbd57a22c3ffa5e35efa8e9b99673c98
Luna <lunaypark@gmail.com>
2022-03-27 21:08:33 -0400

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-// https://javascript.plainenglish.io/create-a-react-app-from-scratch-in-2021-8e9948602e9c
-const App = () => {
-    return <h1>Initial Test</h1>
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
